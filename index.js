f66cc4522e324d8eeac38b5b3d71b098476523b6
Harshitha A R <114691124+harshithaark@users.noreply.github.com>
2025-08-30 10:24:48 +0000

    render changes

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
 
+const express = require("express"); // or `import express from "express";` if using ESM
 const app = express();
-const port = 3000;
-
+const PORT = process.env.PORT || 3000; 
+app.set("view engine", "ejs"); 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -43,6 +44,4 @@ app.post("/", async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
